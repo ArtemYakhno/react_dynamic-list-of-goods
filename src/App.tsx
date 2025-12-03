bb48bd57fa58091767144c52476dc051f192c82f
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-12-03 16:44:13 +0200

    added solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+type Filter = 'all' | 'first5' | 'red';
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: React.FC = () => {
+  const [filter, setFilter] = useState<Filter>('all');
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  useEffect(() => {
+    let loadFn: () => Promise<Good[]>;
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+    switch (filter) {
+      case 'first5':
+        loadFn = get5First;
+        break;
+      case 'red':
+        loadFn = getRedGoods;
+        break;
+      default:
+        loadFn = getAll;
+    }
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    loadFn().then(setGoods);
+  }, [filter]);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button onClick={() => setFilter('all')}>Load all goods</button>
+      <button onClick={() => setFilter('first5')}>Load 5 first goods</button>
+      <button onClick={() => setFilter('red')}>Load red goods</button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
